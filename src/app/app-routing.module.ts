28a4c4c52ee901d27fc50e5a0183a4cd29ce2e48
kyrgansky.ku <kyrgansky.ku@zdmail.ru>
2023-09-12 14:45:39 +0300

    initial project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'todo', loadChildren: () => import('./todo-list/todo-list.module').then(m => m.TodoListModule)},
+  {path: 'login', loadChildren:()=> import('./login-form/login-form.module').then(m=> m.LoginFormModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
